20acc0c620da7a2b445bea2080ef568cc97425e6
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-07-01 15:34:57 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Filipe Fernandes on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
